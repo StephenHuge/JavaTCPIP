d8106abcf6c6d81d77beb5c1be5504902f2cf2cc
HJS <407410014@qq.com>
2018-05-02 17:11:50 +0800

    update packages

---
+++ src/chapter5/tcp/TCPEchoClientNonblocking.java
@@ -0,0 +1,51 @@
+package chapter5.tcp;
+
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class TCPEchoClientNonblocking {
+	public static void main(String[] args) throws Exception {
+		
+		if ((args.length < 2) || (args.length > 3)) {	// 检测输入参数的个数
+			throw new IllegalArgumentException("参数：<Server> <Word> [<Port>]");
+		}
+		
+		String server = args[0];	// 服务器的名称或者IP地址
+		// 使用默认字符集将输入的String转换为字节
+		byte[] argument = args[1].getBytes();
+		
+		int servPort = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
+		
+		// 新建一个Channel，并设置为非阻塞
+		SocketChannel clntChan = SocketChannel.open();
+		clntChan.configureBlocking(false);
+		
+		// 初始化到服务器的连接，由于非阻塞可能在未完成连接直接返回null，所以此处重复轮询直到连接完成
+		// 这种忙等待十分浪费系统资源，此处只是为了演示
+		if (!clntChan.connect(new InetSocketAddress(server, servPort))) {
+			while (!clntChan.finishConnect()) {
+				System.out.println(".");	// 这里表示如果没有完成连接，则可以执行其它任务
+			}
+		}
+		
+		ByteBuffer writeBuf = ByteBuffer.wrap(argument);
+		ByteBuffer readBuf = ByteBuffer.allocate(argument.length);
+		int totalBytesRcvd = 0;				// 目前为止接收的所有字节数
+		int bytesRcvd;						// 上一次读取接收的字符数
+		while (totalBytesRcvd < argument.length) {
+			if (writeBuf.hasRemaining()) {
+				clntChan.write(writeBuf);
+			}
+			if ((bytesRcvd = clntChan.read(readBuf)) == -1) {
+				throw new SocketException("连接已提前关闭");
+			}
+			totalBytesRcvd += bytesRcvd;
+			System.out.println(".");	// 这里表示如果没有读写完毕则可以执行其它任务
+		}
+		System.out.println("收到：" + 		// 按字符转换为字符串
+				new String(readBuf.array(), 0, totalBytesRcvd));
+		clntChan.close();
+	}
+}
---
+++ src/chapter5/tcp/TCPServerSelector.java
@@ -0,0 +1,63 @@
+package chapter5.tcp;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.util.Iterator;
+
+public class TCPServerSelector {
+	
+	private static final int BUFSIZE = 256;		// buffer的字节长度
+	private static final int TIMEOUT = 3000;	// 等待时长（毫秒）
+	
+	public static void main(String[] args) throws IOException {
+		
+		if (args.length < 1) {	// 检验参数的个数
+			throw new IllegalArgumentException("参数：<Port> ...");
+		}
+		
+		// 新建一个Selector用来多路监听socket和连接
+		Selector selector = Selector.open();
+		
+		// 为selector和每个接口创建监听接口的channel
+		for (String arg : args) {
+			ServerSocketChannel listenChannel = ServerSocketChannel.open();
+			listenChannel.socket().bind(new InetSocketAddress(Integer.parseInt(arg)));
+			listenChannel.configureBlocking(false);	// 必须设置为非阻塞才能进行注册
+			// 为Channel注册Selector，返回的key直接忽略
+			listenChannel.register(selector, SelectionKey.OP_ACCEPT);
+		}
+		
+		// 创建一个实现了protocol的handler
+        TCPProtocol protocol = new EchoSelectorProtocol(BUFSIZE);
+
+		while (true) {  // 一直运行，执行可用的I/O操作
+			// 等待channel就绪或者到达等待时间
+			if (selector.select(TIMEOUT) == 0) {    // 返回可用的channel的个数
+                System.out.println(".");
+                continue;
+            }
+
+            // 获取key的迭代器
+            Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+			while (keyIterator.hasNext()) {
+			    SelectionKey key = keyIterator.next();
+			    // 服务器socket是否还有正在处理的任务？
+                if (key.isAcceptable()) {
+                    protocol.handleAccept(key);
+                }
+                // 客户端Socket是否还有正在处理的任务？
+                if (key.isReadable()) {
+                    protocol.handleRead(key);
+                }
+                // 客户端Socket可以写入而且key是有效的
+                if (key.isValid() && key.isWritable()) {
+                    protocol.handleWrite(key);
+                }
+                keyIterator.remove();   // 去掉被选中的key
+            }
+        }
+	}
+}
