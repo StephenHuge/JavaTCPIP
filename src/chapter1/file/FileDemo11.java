c370934375b9140f2ecebdf2a93adfbaaa28f4ad
HJS <407410014@qq.com>
2018-05-10 10:32:53 +0800

    add comments and restructure -- v1.1

--- src/chapter1/file/FileDemo11.java
+++ src/chapter1/file/FileDemo11.java
@@ -1,28 +1,37 @@
 package chapter1.file;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 列出指定目录下所有文件
  */
 public class FileDemo11 {
     public static void main(String[] args) {
-        String dir = File.separator + "home"
-                + File.separator + "hjs";
+        String dir = "d:/WorkEnvironment";
+//		String dir = File.separator + "home"
+//                + File.separator + "hjs";
 //                + File.separator + "Codes"
 //                + File.separator + "TestDir";
 //                + File.separator + "ZipsForAlgorithms";
         File f = new File(dir);
         print(f);
     }
+    /**
+     * 传入根文件名，并初始化各种变量：
+     * 1. Content root ： 根节点
+     * 2. List<Integer> fileNumOfEachLayer (fnel) ： 用来记录每层文件的数量， index表示层数，默认根节点是第0层
+     * 3. int layer ： 表示层数
+     * 
+     * @param file 传入的根文件
+     */
     public static void print(File file) {
         if (file == null) return;
 
         Content root = new Content(null, file, 0, 1);   // 根结点
-        ArrayList<Integer> fileNumOfEachLayer = new ArrayList<>();          // 记录每层文件的数量
-        ArrayList<Integer> fnel = fileNumOfEachLayer;   // 名称缩写
+        List<Integer> fileNumOfEachLayer = new ArrayList<>();          // 记录每层文件的数量
+        List<Integer> fnel = fileNumOfEachLayer;   // 名称缩写
         int layer = 0;                  // 当前所在的层数
 
         fnel.add(layer, 1);     // 将当前层数文件的数量记录在list中
@@ -30,21 +39,22 @@ public class FileDemo11 {
         File thisFile = root.getFile();          // 获取当前节点的文件
         System.out.println(thisFile.getPath());  // 打印当前节点的文件
 
-        String[] sons = thisFile.list();   // 获取当前节点文件的子文件数组
-
+        print(root, fnel);
+        /*
+		String[] sons = thisFile.list();   // 获取当前节点文件的子文件数组
         if (sons != null) {
             fnel.add(layer + 1, sons.length);   // 将下一层文件的数量记录在list中
 
             // 循环下一层所有文件
             for (int i = 0; i < sons.length; i++) {
                 File f = new File(thisFile, sons[i]);
-                Content c = new Content(root, f, layer + 1, i);
-                // 递归执行
+        		Content c = new Content(root, f, layer + 1, i);
+                // 执行打印操作
                 print(c, fnel);
             }
-        }
+        }*/   
     }
-    private static void print(Content c, ArrayList<Integer> fnel) {
+    private static void print(Content c, List<Integer> fnel) {
         if (c == null || fnel == null)  return;
         File f = c.getFile();       // 获取当前节点文件
         String[] sons = f.list();   // 获取当前节点文件的子文件数组
@@ -60,7 +70,7 @@ public class FileDemo11 {
         } else {
             System.out.print("- ");
         }
-        String[] ss = c.getFile().getPath().split("/");
+        String[] ss = c.getFile().getPath().split("\\\\");
         System.out.println(ss[ss.length - 1]);
 
         // 如果此文件为文件夹，继续打印下一层
@@ -74,7 +84,7 @@ public class FileDemo11 {
             }
         }
     }
-    private static void print(Content c, ArrayList<Integer> fnel, int non) {
+    private static void print(Content c, List<Integer> fnel, int non) {
         // 递归直到root下一层
         if (c.getParent() != null) {
             print(c.getParent(), fnel, non);
