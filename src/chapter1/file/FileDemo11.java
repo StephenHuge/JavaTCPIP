5d14c67cfcbea4e4356531cc73e99b99bbb0a80c
HJS <407410014@qq.com>
2018-05-10 10:51:31 +0800

    reconstruct code -- v1.2

--- src/chapter1/file/FileDemo11.java
+++ src/chapter1/file/FileDemo11.java
@@ -8,199 +8,107 @@ import java.util.List;
  * 列出指定目录下所有文件
  */
 public class FileDemo11 {
-    public static void main(String[] args) {
-        String dir = "d:/WorkEnvironment";
-//		String dir = File.separator + "home"
-//                + File.separator + "hjs";
-//                + File.separator + "Codes"
-//                + File.separator + "TestDir";
-//                + File.separator + "ZipsForAlgorithms";
-        File f = new File(dir);
-        print(f);
-    }
-    /**
-     * 传入根文件名，并初始化各种变量：
-     * 1. Content root ： 根节点
-     * 2. List<Integer> fileNumOfEachLayer (fnel) ： 用来记录每层文件的数量， index表示层数，默认根节点是第0层
-     * 3. int layer ： 表示层数
-     * 
-     * @param file 传入的根文件
-     */
-    public static void print(File file) {
-        if (file == null) return;
+	public static void main(String[] args) {
+		String dir = "d:/WorkEnvironment";
+		File f = new File(dir);
+		print(f);
+	}
+	/**
+	 * 传入根文件名，并初始化各种变量：
+	 * 1. Content root ： 根节点
+	 * 2. List<Integer> fileNumOfEachLayer (fnel) ： 用来记录每层文件的数量，
+	 *    index表示层数，默认根节点是第0层
+	 * 3. int layer ： 表示层数
+	 * 
+	 * @param file 传入的根文件
+	 */
+	public static void print(File file) {
+		if (file == null) return;
 
-        Content root = new Content(null, file, 0, 1);   // 根结点
-        List<Integer> fileNumOfEachLayer = new ArrayList<>();          // 记录每层文件的数量
-        List<Integer> fnel = fileNumOfEachLayer;   // 名称缩写
-        int layer = 0;                  // 当前所在的层数
+		Content root = new Content(null, file, 0, 1);   // 根结点
+		List<Integer> fileNumOfEachLayer = new ArrayList<>();	// 记录每层文件的数量
+		List<Integer> fnel = fileNumOfEachLayer;   // 名称缩写
+		int layer = 0;          // 当前所在的层数
 
-        fnel.add(layer, 1);     // 将当前层数文件的数量记录在list中
+		fnel.add(layer, 1);     // 将当前层数文件的数量记录在list中
+		File thisFile = root.getFile();          // 获取根节点的文件
+		System.out.println(thisFile.getPath());  // 打印根节点的文件
 
-        File thisFile = root.getFile();          // 获取当前节点的文件
-        System.out.println(thisFile.getPath());  // 打印当前节点的文件
+		print(root, fnel);
+	}
+	private static void print(Content c, List<Integer> fnel) {
+		if (c == null || fnel == null)  return;
+		File f = c.getFile();       // 获取当前节点文件
+		File[] sonFiles = f.listFiles();   // 获取当前节点文件的子文件数组
+		
+		print(c, fnel, c.getLayer());		// 判断如何打印并打印当前行的目录结构
+		// 判断目录是否为空或者是否是文件
+		System.out.print((c.getFile().list() != null) ? "+ " : "- ");	
+		
+		System.out.println(c.getFile().getName());		// 打印文件名
+		
+		// 如果此文件为文件夹，继续打印下一层
+		if (sonFiles != null) {
+			fnel.add(c.getLayer() + 1, sonFiles.length);
+			for (int i = 0; i < sonFiles.length; i++) {
+				Content sonContent = new Content(c, sonFiles[i], c.getLayer() + 1, i);
+				print(sonContent, fnel);	// 递归执行
+			}
+		}
+	}
+	private static void print(Content c, List<Integer> fnel, int non) {
+		// 递归直到root下一层
+		if (c.getParent() != null) {
+			print(c.getParent(), fnel, non);
+		}
+		if (c.getLayer() == non)    return;
+		
+		// 判断当前文件是否是自己所在层的最后一个文件
+		System.out.print((fnel.get(c.getLayer()) - 1 > c.getId()) ? "│ " : "  ");
+	}
+	static class Content {
+		private Content parent;
+		private File file;
+		private int layer;      // 相对于根目录此文件的层数
+		private int id;   // 本层中所有的文件数，此变量是为了判定最后一个文件用的
 
-        print(root, fnel);
-        /*
-		String[] sons = thisFile.list();   // 获取当前节点文件的子文件数组
-        if (sons != null) {
-            fnel.add(layer + 1, sons.length);   // 将下一层文件的数量记录在list中
+		public Content(Content parent, File file, int layer, int id) {
+			this.parent = parent;
+			this.file = file;
+			this.layer = layer;
+			this.id = id;
+		}
 
-            // 循环下一层所有文件
-            for (int i = 0; i < sons.length; i++) {
-                File f = new File(thisFile, sons[i]);
-        		Content c = new Content(root, f, layer + 1, i);
-                // 执行打印操作
-                print(c, fnel);
-            }
-        }*/   
-    }
-    private static void print(Content c, List<Integer> fnel) {
-        if (c == null || fnel == null)  return;
-        File f = c.getFile();       // 获取当前节点文件
-        String[] sons = f.list();   // 获取当前节点文件的子文件数组
-        // 判断如何打印并打印当前行
-//        System.out.print(
-//                String.format("(%d, %d)",
-//                        c.getLayer(), c.getId())
-//        );
+		public Content getParent() {
+			return parent;
+		}
 
-        print(c, fnel, c.getLayer());
-        if (c.getFile().list() != null) {
-            System.out.print("+ ");
-        } else {
-            System.out.print("- ");
-        }
-        String[] ss = c.getFile().getPath().split("\\\\");
-        System.out.println(ss[ss.length - 1]);
+		public void setParent(Content parent) {
+			this.parent = parent;
+		}
 
-        // 如果此文件为文件夹，继续打印下一层
-        if (sons != null) {
-            fnel.add(c.getLayer() + 1, sons.length);
-            for (int i = 0; i < sons.length; i++) {
-                File sonFile = new File(f, sons[i]);
-                Content sonContent = new Content(c, sonFile, c.getLayer() + 1, i);
-                // 递归执行
-                print(sonContent, fnel);
-            }
-        }
-    }
-    private static void print(Content c, List<Integer> fnel, int non) {
-        // 递归直到root下一层
-        if (c.getParent() != null) {
-            print(c.getParent(), fnel, non);
-        }
+		public File getFile() {
+			return file;
+		}
 
-        if (c.getLayer() == non)    return;
-        // 打印当前行
-//        System.out.print(String.format("%d ~ %d", fnel.get(c.getLayer()) - 1, c.getId()));
-//        System.out.print(String.format("%d ~ %d", fnel.get(c.getLayer()) - 1, c.getLayer()));
-        if (fnel.get(c.getLayer()) - 1 > c.getId() ) {   // 判断当前文件是否是自己所在层的最后一个文件
-            System.out.print("│ ");
-        } else {
-            System.out.print("  ");
-        }
-    }
-static class Content {
-    private Content parent;
-    private File file;
-    private int layer;      // 相对于根目录此文件的层数
-    private int id;   // 本层中所有的文件数，此变量是为了判定最后一个文件用的
+		public void setFile(File file) {
+			this.file = file;
+		}
 
-    public Content(Content parent, File file, int layer, int id) {
-        this.parent = parent;
-        this.file = file;
-        this.layer = layer;
-        this.id = id;
-    }
+		public int getLayer() {
+			return layer;
+		}
 
-    public Content getParent() {
-        return parent;
-    }
+		public void setLayer(int layer) {
+			this.layer = layer;
+		}
 
-    public void setParent(Content parent) {
-        this.parent = parent;
-    }
+		public int getId() {
+			return id;
+		}
 
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
-    }
-
-    public int getLayer() {
-        return layer;
-    }
-
-    public void setLayer(int layer) {
-        this.layer = layer;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+		public void setId(int id) {
+			this.id = id;
+		}
+	}
 }
-    /*public static void print(File file, int layer, boolean flag) {
-        if (file != null) {
-            layer++;
-            String[] strings = file.list();
-            if (strings != null) {
-
-                for (int i = 0; i < strings.length; i++) {
-                    if (strings[i].startsWith(".")) continue;
-                    if (strings[i].endsWith(".class")) continue;
-                    if (strings[i].equals("EclipseWorkSpace")
-                            || strings[i].equals("ZipsForAlgorithms")
-                            || strings[i].equals("IDEAWorkSpace")) continue;
-                    int a = layer;
-                    if (i != strings.length - 1) flag = true;
-//                        if (a != 1) System.out.print("│ ");
-                    while (a != 0) {
-                        if (a != layer && flag) {
-                            System.out.print("│ ");
-                        }
-                        else {
-                            System.out.print("  ");
-                        }
-                        a--;
-                    }
-                    System.out.print("- ");
-                    System.out.print(strings[i]);
-                    System.out.println(
-                            String.format(" - (layer : %d, length : %d, i : %d, flag : %b)",
-                                    layer, strings.length, i, flag)
-                    );
-                    File f = new File(file, strings[i]);
-                    if (i == strings.length - 1) flag = false;
-                    print(f, layer, flag);
-                }
-            }
-        }
-    }*/
-}
- /*layer = layer << 1 | 0x1;
-            System.out.println(Integer.toBinaryString(layer));
-            String[] strings = file.list();
-            int l = layer;  // 临时变量
-            for (int i = 0; i < strings.length; i++) {
-                int m = Byte.SIZE;
-                while (m-- > 0) {
-                    if ((l & 0x80) == 0x80) {
-                        System.out.print("│ ");
-                    } else {
-                        System.out.print("  ");
-                    }
-                    l <<= 1;
-                }
-                System.out.print("- ");
-                System.out.println(strings[i]);
-
-                File f = new File(file, strings[i]);
-                print(f, layer);
-            }*/
-/*  */
