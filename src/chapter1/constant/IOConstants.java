07475ab73065cddb7a560aa96b3e0eeabc058878
HJS <407410014@qq.com>
2018-05-12 22:38:28 +0800

    add new constants and add README.md

--- src/chapter1/constant/IOConstants.java
+++ src/chapter1/constant/IOConstants.java
@@ -6,4 +6,8 @@ public class IOConstants {
     public static final String PATH_CODES = File.separator + "home"
             + File.separator + "hjs"
             + File.separator + "Codes";
+    public static final String PATH_TEST_TXT = PATH_CODES
+            + File.separator + "test.txt";
+    public static final String PATH_ERR_LOG = PATH_CODES
+            + File.separator + "err.log";
 }
